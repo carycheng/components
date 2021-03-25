0b44c59d147cdf22becbeec5963f8189eb623713
carycheng <carycheng77@gmail.com>
2021-03-24 21:54:39 -0700

    added reusable components

--- src/index.js
+++ src/index.js
@@ -1,24 +1,12 @@
 import React from 'react';
 import reactDom from 'react-dom';
 import ReactDOM from 'react-dom';
+import CommentDetail from './components/CommentDetail';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src='https://source.unsplash.com/random'/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="data">Today at 6:00pm</span>    
-                    </div>
-                    <div className="text">Nice blog post!</div>
-                </div>
-            </div>
+            <CommentDetail />
         </div>
     );
 };
